9f561e1171fcde5a99c822799cf7c0915f449ee4
Disego <disegorachelm@gmail.com>
2024-03-27 11:24:26 +0200

    fixed function 1

---
+++ script.js
@@ -0,0 +1,43 @@
+let state = 'idle'
+let user = null
+let calculated = '1'
+
+// Only allowed to change below
+
+
+function logCalc () { 
+    const isString = typeof calculated === 'numerical-string'; 
+    const calculatedAsNumber = isString ? parseInt(calculated) : calculated;   //! converts the numeric string into a number
+    calculated = calculatedAsNumber + 1;
+    return calculated;
+}
+
+// function calcUser () {
+//   logCalc
+//   if (calculated > 2) user = 'John'
+//   if (calculated > 2) state = 'requesting'
+//   if (calculated > 3) state = 'idle'
+// }
+
+// function checkUser () {
+// 	if (user && state === 'requesting') {
+// 		console.log(`User: ${user} (${calculated})`)
+// 	}
+// }
+
+// // Only allowed to change code above
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
+
+// checkUser()
+// calcUser()
